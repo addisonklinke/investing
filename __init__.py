0f6f85bd7778e33d03ace437f2a587ed07cbc517
Addison Klinke <addison@openalpr.com>
2019-05-04 18:30:24 -0600

    both Whale Wisdom keys

--- __init__.py
+++ __init__.py
@@ -2,5 +2,7 @@ from pickle import load
 
 keys = {
     'alpha-vantage': '26FXSXCVGD0QZZ3M',
-    'whale-wisdom': 'oaHbKtuUdWkcXCsw5jk4'}
+    'whale-wisdom': {
+        'shared': 'oaHbKtuUdWkcXCsw5jk4',
+        'secret': 's0zpLNkpI7GBKz2chiABMJv97eGNdIyQyjohrHQh'}}
 ticker_to_name = load(open('./data/ticker_to_name.pkl', 'rb'))

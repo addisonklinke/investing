e184a6691da336ce4bd6a8d4134d0822f6543afe
Addison Klinke <addison@openalpr.com>
2019-05-06 16:43:40 -0600

    successfully generate WhaleWisdom signature hash

--- download.py
+++ download.py
@@ -1,8 +1,24 @@
+import base64
+import hashlib
+import hmac
 import json
 import os
 import pickle
+import time
+from urllib.parse import quote_plus
 import requests
 from investing import keys
+
+
+def holdings(company='berkshire'):
+    url = 'https://whalewisdom.com/shell/command.json?args={}&api_shared_key={}&api_sig={}&timestamp={}'
+    url_json = '{"command":"quarters"}'
+    now = time.strftime('%Y-%m-%dT%H:%M:%SZ', time.gmtime())
+    sig = _whale_wisdom_signature(url_json, now)
+    api_url = url.format(quote_plus(url_json), keys['whale-wisdom']['shared'], sig, now)
+    r = requests.get(api_url)
+    print(r.status_code)
+    print(r.content)
 
 
 def timeseries(symbol, length='compact', out_dir='/mnt/stocks'):
@@ -30,3 +46,20 @@ def timeseries(symbol, length='compact', out_dir='/mnt/stocks'):
         pickle.dump(current, open(fpath, 'wb'))
     else:
         pickle.dump(ts, open(fpath, 'wb'))
+
+
+def _whale_wisdom_signature(url_json, now):
+    """Create hash from secret key based on Whale Wisdom procedure.
+
+    See https://whalewisdom.com/python3_api_sample.txt
+
+    :param str url_json: Parameters for API endpoint.
+    :param str now: Timestamp string for the request.
+    :return str sig: Constructed using HMAC-SHA1 algorithm.
+    """
+    key = keys['whale-wisdom']['secret'].encode()
+    digest = hashlib.sha1
+    hash_args = '{}\n{}'.format(url_json, now).encode()
+    hmac_hash = hmac.new(key, hash_args, digest).digest()
+    sig = base64.b64encode(hmac_hash).rstrip().decode()
+    return sig

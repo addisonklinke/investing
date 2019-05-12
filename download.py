87dd9ddde0b0997284121c16a3d1e0ee5a2cdd22
Addison Klinke <addison@openalpr.com>
2019-05-12 12:35:05 -0600

    switch holdings download from Whale Wisdom to Dataroma

--- download.py
+++ download.py
@@ -1,67 +1,32 @@
-import base64
-import hashlib
-import hmac
 import json
 import os
 import pickle
-import time
 from warnings import warn
+import pandas as pd
 import requests
 from investing import keys, endpoints
 
 
-def filer_id(company):
-    """Get Whale Wisdom internal ID for filer.
-
-    :param str company: Name of the company (does not have to be exact).
-    :return int id: None if no matches were found.
-    """
-    url_json = '{{"command":"filer_lookup", "name":"{}"}}'.format(company)
-    sig, now = _whale_wisdom_signature(url_json)
-    r = requests.get(
-        url=endpoints['whale-wisdom'],
-        params={
-            'args': url_json,
-            'api_shared_key': keys['whale-wisdom']['shared'],
-            'api_sig': sig,
-            'timestamp': now})
-    data = json.loads(r.content)
-    if len(data['filers']) > 1:
-        warn('Multiple matches for {}, defaulting to first'.format(company))
-    if len(data['filers']) > 0:
-        id = data['filers'][0]['id']
-    else:
-        id = None
-    return id
-
-
-def holdings(company='BERKSHIRE HATHAWAY INC'):
+def holdings(ticker='BRK'):
     """Save list of stock holdings for a particular company.
 
-    :param str company: Name of the company (does not have to be exact).
-    :return: None
+    :param str ticker: Company ticker (case insensitive).
+    :return [str] held_tickers: Tickers of company holdings.
     """
-    id = filer_id(company)
-    url_json = '{{"command":"holdings", "filer_ids":[{}]}}'.format(id)
-    sig, now = _whale_wisdom_signature(url_json)
-    r = requests.get(
-        url=endpoints['whale-wisdom'],
-        params={
-            'args': url_json,
-            'api_shared_key': keys['whale-wisdom']['shared'],
-            'api_sig': sig,
-            'timestamp': now})
-    print(r.status_code)
-    print(r.content)
+    r = requests.get(endpoints['dataroma'], {'m': ticker})
+    tables = pd.read_html(r.content)
+    holdings = tables[0]
+    held_tickers = [s.split('-')[0].strip() for s in holdings.Stock]
+    return held_tickers
 
 
-def stock_news(ticker=None, items=50):
+def news(ticker=None, items=50):
     """Gather news articles for general market or a specific ticker.
 
     :param str ticker: Defaults to ``None`` for general market news.
     :param int items: Number of articles to return (max allowed for free
         subscription is 50).
-    :return:
+    :return dict articles: JSON formatted news articles.
     """
     if items > 50:
         warn('More than 50 items is not supported by free tier')
@@ -73,27 +38,33 @@ def stock_news(ticker=None, items=50):
         params = {'section': 'alltickers'}
     params.update({'items': items, 'token': keys['stock-news']})
     r = requests.get(url, params)
-    data = json.loads(r.content)
-    negative = [(i, d, d['tickers']) for i, d in enumerate(data['data']) if d['sentiment'] == 'Negative']
+    articles = json.loads(r.content)
+    return articles
 
-def timeseries(symbol, length='compact', out_dir='/mnt/stocks'):
+
+def timeseries(ticker, length='compact', out_dir='/mnt/stocks'):
     """Save JSON formatted pickle of stock prices from Alpha Vantage API.
 
-    If symbol exists locally, new results will be appended to the current data
+    If ticker exists locally, new results will be appended to the current data
 
-    :param str symbol: Uppercase stock abbreviation.
+    :param str ticker: Uppercase stock abbreviation.
     :param str length: Either compact (last 100 days) or full (20 years).
     :param str out_dir: Directory to save pickle file
     :return: None
     """
-    url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={}&outputsize={}&apikey={}'
-    r = requests.get(url.format(symbol, length, keys['alpha-vantage']))
+    r = requests.get(
+        url=endpoints['alpha-vantage'],
+        params={
+            'function': 'TIME_SERIES_DAILY',
+            'symbol': ticker.upper(),
+            'outputsize': length,
+            'apikey': keys['alpha-vantage']})
     data = json.loads(r.content)
     try:
         ts = {k: float(v['4. close']) for k, v in data['Time Series (Daily)'].items()}
     except KeyError:
-        raise ValueError('Invalid stock symbol {}'.format(symbol))
-    fpath = os.path.join(out_dir, '{}.pkl'.format(symbol.lower()))
+        raise ValueError('Invalid stock ticker {}'.format(ticker))
+    fpath = os.path.join(out_dir, '{}.pkl'.format(ticker.lower()))
     if os.path.exists(fpath):
         current = pickle.load(open(fpath, 'rb'))
         new = {k: v for k, v in ts.items() if k > max(current.keys())}
@@ -101,21 +72,3 @@ def timeseries(symbol, length='compact', out_dir='/mnt/stocks'):
         pickle.dump(current, open(fpath, 'wb'))
     else:
         pickle.dump(ts, open(fpath, 'wb'))
-
-
-def _whale_wisdom_signature(url_json):
-    """Create hash from secret key based on Whale Wisdom procedure.
-
-    See https://whalewisdom.com/python3_api_sample.txt
-
-    :param str url_json: Parameters for API endpoint.
-    :param str now: Timestamp string for the request.
-    :return tuple: Signature from HMAC-SHA1 algorithm and timestamp.
-    """
-    key = keys['whale-wisdom']['secret'].encode()
-    digest = hashlib.sha1
-    now = time.strftime('%Y-%m-%dT%H:%M:%SZ', time.gmtime())
-    hash_args = '{}\n{}'.format(url_json, now).encode()
-    hmac_hash = hmac.new(key, hash_args, digest).digest()
-    sig = base64.b64encode(hmac_hash).rstrip().decode()
-    return sig, now

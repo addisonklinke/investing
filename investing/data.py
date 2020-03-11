0134a0914751f16098720b6b2ddc7ccdfc5f1e2b
Addison Klinke <addison@openalpr.com>
2020-03-10 22:02:25 -0600

    add checker for market days and stale local data

--- investing/data.py
+++ investing/data.py
@@ -4,7 +4,38 @@ import numpy as np
 import pandas as pd
 from . import conf
 from .mappings import ticker2name
-from .utils import parse_period
+
+
+def parse_period(period):
+    """Convert various financial periods to number of days
+
+    :param int or str period: Number of days for the return window or one of
+        the following keyword strings
+            * daily
+            * monthly
+            * quarterly
+            * yearly
+            * n-year
+    :return int days:
+    """
+    if isinstance(period, int):
+        days = period
+    elif isinstance(period, str):
+        if period == 'daily':
+            days = 1
+        elif period == 'monthly':
+            days = 30
+        elif period == 'quarterly':
+            days = 91
+        elif period == 'yearly':
+            days = 365
+        elif period.endswith('year') and '-' in period:
+            days = int(period.split('-')[0]) * 365
+        else:
+            raise ValueError(f'{period} string does not match supported formats')
+    else:
+        raise ValueError(f'Exepcted type int or str, but received {type(period)}')
+    return days
 
 
 class Ticker:
--- investing/utils.py
+++ investing/utils.py
@@ -1,36 +1,44 @@
 import argparse
+from datetime import date, datetime, timedelta
+import pandas_market_calendars as mcal
+import pytz
+from . import conf
+from .data import Ticker
 
 
-def parse_period(period):
-    """Convert various financial periods to number of days
+def is_current(ticker):
+    """Check if ticker CSV has the most recent data
 
-    :param int or str period: Number of days for the return window or one of
-        the following keyword strings
-            * daily
-            * monthly
-            * quarterly
-            * yearly
-            * n-year
-    :return int days:
+    :param str ticker: Ticker symbol to check
+    :return bool: Whether the latest timestamp matches the last market day
     """
-    if isinstance(period, int):
-        days = period
-    elif isinstance(period, str):
-        if period == 'daily':
-            days = 1
-        elif period == 'monthly':
-            days = 30
-        elif period == 'quarterly':
-            days = 91
-        elif period == 'yearly':
-            days = 365
-        elif period.endswith('year') and '-' in period:
-            days = int(period.split('-')[0]) * 365
-        else:
-            raise ValueError(f'{period} string does not match supported formats')
+    latest_close = market_day('current')
+    t = Ticker(ticker)
+    return latest_close <= t.data.date.max()
+
+
+def market_day(direction):
+    """Return the closest completed and valid market day
+
+    :param str direction: One of previous, current, next
+    :return np.datetime64: Date stamp
+    """
+    nyse = mcal.get_calendar('NYSE')
+    recent = nyse.valid_days(start_date=date.today() - timedelta(days=7), end_date=date.today() + timedelta(days=1))
+    idx = -2  # Last series index is the 1 day in the future
+    current_close = nyse.schedule(start_date=recent[idx], end_date=recent[idx]).market_close[0]
+    tz = pytz.timezone(conf['locale'])
+    if current_close > datetime.now(tz):
+        idx -= 1
+    if direction == 'current':
+        stamp = recent[idx]
+    elif direction == 'previous':
+        stamp = recent[idx - 1]
+    elif direction == 'next':
+        stamp = recent[idx + 1]
     else:
-        raise ValueError(f'Exepcted type int or str, but received {type(period)}')
-    return days
+        raise ValueError(f'Expected direction to be previous, current, or next, but received {direction}')
+    return stamp.to_numpy()
 
 
 def ptable_to_csv(table, filename, headers=True):
